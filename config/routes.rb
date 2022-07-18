b6463224642ab07af2e602e41d31996634290fce
rutiksimform <rutik.p@simformsolutions.com>
2022-07-18 14:50:03 +0530

    style devise resend-confirmation view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     passwords: "users/passwords",
     registrations: "users/registrations",
+    confirmations: "users/confirmations",
    }
   root "homes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
