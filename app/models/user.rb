9de194593a7a9ef49b1ac0c4eed3b83adcedbd4c
rutiksimform <rutik.p@simformsolutions.com>
2022-07-18 15:27:20 +0530

    add welcome mail after confirming account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  private
+
+  def after_confirmation
+    WelcomeMailer.send_greetings_notification(self).deliver_now
+  end
 end
