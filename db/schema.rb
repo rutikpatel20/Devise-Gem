591a32cfe86c34c1669940a3b818d695f74ee86a
rutiksimform <rutik.p@simformsolutions.com>
2022-07-19 11:16:04 +0530

    add custom fields to new registration form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_082836) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_122500) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,6 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_18_082836) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
