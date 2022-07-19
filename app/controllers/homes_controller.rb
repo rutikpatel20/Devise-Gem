6147549d5ee9bb76808bf37e87707c3e0ea29892
rutiksimform <rutik.p@simformsolutions.com>
2022-07-19 12:51:30 +0530

    custom path added after signup

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def index
   end
+
+  def after_registration_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { 
-    sessions: "users/sessions",
-    passwords: "users/passwords",
-    registrations: "users/registrations",
-    confirmations: "users/confirmations",
-   }
+  devise_for :users, controllers: {
+                       sessions: "users/sessions",
+                       passwords: "users/passwords",
+                       registrations: "users/registrations",
+                       confirmations: "users/confirmations",
+                     }
   root "homes#index"
+
+  get "/confirmation_pending" => "homes#after_registration_path"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
