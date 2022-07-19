77fa6df17b28f04d4db18f14eeb824dac8959711
rutiksimform <rutik.p@simformsolutions.com>
2022-07-19 11:22:10 +0530

    username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
+  validates :username, presence: true, uniqueness: true
+
   private
 
   def after_confirmation
